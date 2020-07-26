a76c686b505978ace624782f91d70349e545e630
ggrassiant <guillaume.grassiant@gmail.com>
2020-07-25 23:01:15 -0400

    adjust cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CLIENT_HOST']
+    # origins ENV['CLIENT_HOST']
+    origins '*'
 
     resource '*',
              headers: :any,
