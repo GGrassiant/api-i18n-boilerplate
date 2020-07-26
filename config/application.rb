8bbadd5f1a6b683042f7e4d66419e43f9d6a1656
ggrassiant <guillaume.grassiant@gmail.com>
2020-07-25 22:27:51 -0400

    adjust app name

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ApiI18nBoiplate
+module ApiI18nBoilerplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
